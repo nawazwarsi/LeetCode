d6f4aaa4607d70675aeed3de86d7e99fd7c86ba6
Nawaz Warsi <nawaz.khan2525@gmail.com>
2022-02-02 23:08:30 +0530

    Time: 32 ms (5.27%), Space: 14 MB (57.89%) - LeetHub

--- 2011-final-value-of-variable-after-performing-operations/2011-final-value-of-variable-after-performing-operations.cpp
+++ 2011-final-value-of-variable-after-performing-operations/2011-final-value-of-variable-after-performing-operations.cpp
@@ -3,17 +3,11 @@ public:
     int finalValueAfterOperations(vector<string>& operations) {
         int X=0;
         for(int i=0; i<operations.size(); i++){
-            if(operations[i]=="++X"){
+            if(operations[i]=="++X" || operations[i]=="X++"){
                 ++X;
             }
-            else if(operations[i]=="X++"){
-                X++;
-            }
-            else if(operations[i]=="--X"){
+            else if(operations[i]=="--X" || operations[i]=="X--"){
                 --X;
-            }
-            else{
-                X--;
             }
         }
         return X;
