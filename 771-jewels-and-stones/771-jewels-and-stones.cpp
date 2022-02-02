af3fa0aeb8f550b717f43106220050b254d3fbf4
Nawaz Warsi <nawaz.khan2525@gmail.com>
2022-02-02 23:56:16 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (55.03%) - LeetHub

--- 771-jewels-and-stones/771-jewels-and-stones.cpp
+++ 771-jewels-and-stones/771-jewels-and-stones.cpp
@@ -2,9 +2,9 @@ class Solution {
 public:
     int numJewelsInStones(string jewels, string stones) {
         int count=0;
-        for(char i : jewels){
-            for(char j : stones){
-                if(i==j){
+        for(int i=0; i<jewels.size(); i++){
+            for(int j=0; j<stones.size(); j++){
+                if(jewels[i]==stones[j]){
                     count++;
                 }
             }
