ca55af25084f1c54cff8cec21f2320ca06483f2e
Nawaz Warsi <nawaz.khan2525@gmail.com>
2022-02-03 00:27:12 +0530

    Time: 25 ms (16.21%), Space: 9.7 MB (95.26%) - LeetHub

---
+++ 2114-maximum-number-of-words-found-in-sentences/2114-maximum-number-of-words-found-in-sentences.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int mostWordsFound(vector<string>& sentences) {
+        int ans=0,count=0;
+        for(int i=0; i<sentences.size(); i++){
+            for(int j=0; j<sentences[i].size(); j++){
+                if(sentences[i][j]==' '){
+                    count++; 
+                }
+                ans=max(count,ans);
+            }
+            count=0;
+        }
+        return ans+1;
+    }
+};
