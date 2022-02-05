2e5fbee688b11cbee57d591c0c0145a6ce0cb72d
Nawaz Warsi <nawaz.khan2525@gmail.com>
2022-02-05 18:39:26 +0530

    Time: 19 ms (74.64%), Space: 19.7 MB (51.72%) - LeetHub

---
+++ 169-majority-element/169-majority-element.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+     sort(nums.begin(),nums.end());
+        
+     int n=nums.size();
+        if(n<=1){
+            return nums[0];
+        }
+     int count=1;
+     for(int i=1;i<n;i++){
+         if(nums[i]==nums[i-1]){
+             count++;
+             if(count>n/2)
+             return nums[i];
+         }
+         else{
+             count=1;
+         }
+     }
+        return -1;
+    }
+};
