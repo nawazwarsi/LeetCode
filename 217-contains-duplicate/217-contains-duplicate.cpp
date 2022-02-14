0ec802ed36ee2b63878bca3437efee59918a4bb7
Nawaz Warsi <nawaz.khan2525@gmail.com>
2022-02-14 12:12:39 +0530

    Time: 88 ms (82.70%), Space: 46.7 MB (75.18%) - LeetHub

---
+++ 217-contains-duplicate/217-contains-duplicate.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        bool flag;
+     sort(nums.begin(),nums.end());
+        for(int i=1; i<nums.size(); i++){
+            if(nums[i]==nums[i-1]){
+                return true;
+            }  
+        }
+            return false;
+    }
+};
