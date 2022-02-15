f351834fb9de0f504a80d3f3c8946063bb20625c
Nawaz Warsi <nawaz.khan2525@gmail.com>
2022-02-15 08:48:11 +0530

    Time: 30 ms (45.33%), Space: 17 MB (50.83%) - LeetHub

--- 136-single-number/136-single-number.cpp
+++ 136-single-number/136-single-number.cpp
@@ -2,7 +2,6 @@ class Solution {
 public:
     int singleNumber(vector<int>& nums) {
         int ans=0;
-        sort(nums.begin(),nums.end());
         for(int i=0; i<nums.size(); i++){
             ans^=nums[i];
         }
