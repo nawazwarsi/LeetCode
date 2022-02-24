c9f4d286513acb80015e9c28f1890d24e9c0a37f
Nawaz Warsi <nawaz.khan2525@gmail.com>
2022-02-24 18:27:29 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (69.81%) - LeetHub

--- 1108-defanging-an-ip-address/1108-defanging-an-ip-address.cpp
+++ 1108-defanging-an-ip-address/1108-defanging-an-ip-address.cpp
@@ -1,8 +1,10 @@
 class Solution {
 public:
     string defangIPaddr(string address) {
-        string ans="";
-        for(int i=0; i<address.length(); i++){
+        int n=address.size();
+        int i=0;
+        string ans;
+        for(int i=0; i<n; i++){
             if(address[i]=='.'){
                 ans+="[.]";
             }
