97d2402e24224db0c565797a2c9f21ef5bffc675
Nawaz Warsi <nawaz.khan2525@gmail.com>
2022-02-26 20:19:20 +0530

    Time: 23 ms (50.79%), Space: 8.9 MB (16.00%) - LeetHub

---
+++ 383-ransom-note/383-ransom-note.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+        unordered_map<char,int> mp;
+        for(int i =0;i<magazine.length();i++)
+        {
+            mp[magazine[i]]++;
+        }
+        
+        for(int i = 0;i<ransomNote.length();i++)
+        {
+            if(mp[ransomNote[i]]<1)
+                return false;
+            else
+                mp[ransomNote[i]]--;
+        }
+        return true;
+    }
+};
