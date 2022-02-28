1572d7ddc62630154c9b939e19479c6f03f6d82b
Nawaz Warsi <nawaz.khan2525@gmail.com>
2022-02-28 08:33:49 +0530

    Time: 0 ms (100.00%), Space: 6.8 MB (62.02%) - LeetHub

--- 228-summary-ranges/228-summary-ranges.cpp
+++ 228-summary-ranges/228-summary-ranges.cpp
@@ -1,39 +1,18 @@
 class Solution {
 public:
     vector<string> summaryRanges(vector<int>& nums) {
-		// resultant string
-        vector<string> result;
+        int n = nums.size(), s= 0, e = 0;
+        vector<string> ans;
+        if(n==0) return ans;
         
-        int n = nums.size();
-		// if size happens to be  zero return empty string
-        if(n == 0 )
-                return result;
-        
-		// assigning first element to a
-        int a = nums[0];
-        
-        for(int i = 0; i<n; i++)
-        {
-			// if one of both is true
-            if( i == n-1 || nums[i]+1 != nums[i+1])
-            {
-			    // if current element is not equals a
-				// this means we have found a range.
-                if(nums[i] != a)
-                    result.push_back(to_string(a)+ "->"+ to_string(nums[i]));
-					
-				// this means we have reached to the end of string and now
-				// we have to add a that should be the last element
-                else
-                        result.push_back(to_string(a));
-						
-				// checking  for this condition so that a got updated for next range
-				// also n-1 so that a doesn't contain out of bound value
-                if(i != n-1)
-                    a = nums[i+1];
-            }
+        for(int i=0; i<n; i=e+1){
+            s = e = i;
+            while(e+1 < n && nums[e+1]==nums[e]+1) e++;
+            if(e > s) 
+                ans.push_back(to_string(nums[s]) + "->" + to_string(nums[e]));
+            else 
+                ans.push_back(to_string(nums[s]));
         }
-		// return result
-        return result;
+        return ans;
     }
 };
