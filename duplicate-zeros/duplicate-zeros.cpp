81e96f4743fad1023351d43b23282659cd6284db
Nawaz Warsi <nawaz.khan2525@gmail.com>
2022-03-07 14:55:39 +0530

    Time: 45 ms, Memory: 9.7 MB - LeetHub

--- duplicate-zeros/duplicate-zeros.cpp
+++ duplicate-zeros/duplicate-zeros.cpp
@@ -1,17 +1,11 @@
 class Solution {
 public:
     void duplicateZeros(vector<int>& arr) {
-        
-        int n=arr.size();
-        
-        for(int i=0;i<n;i++)
-        {
-            if(arr[i]==0)
-            {
-                arr.erase(arr.begin()+n-1);
+        for(int i=0; i<arr.size();++i){
+            if(arr[i]==0){
+                arr.pop_back();
                 arr.insert(arr.begin()+i,0);
-                
-                i++;
+                ++i;
             }
         }
     }
