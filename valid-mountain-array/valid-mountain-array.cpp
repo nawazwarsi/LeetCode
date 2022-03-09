ffdae1c9f066274b675da9a36c9b5f1c4a8ef482
Nawaz Warsi <nawaz.khan2525@gmail.com>
2022-03-09 12:07:55 +0530

    Time: 41 ms, Memory: 22.4 MB - LeetHub

--- valid-mountain-array/valid-mountain-array.cpp
+++ valid-mountain-array/valid-mountain-array.cpp
@@ -2,14 +2,14 @@ class Solution {
 public:
     bool validMountainArray(vector<int>& arr) {
         if(arr.size() < 3) return false;
-        int l = 0;
-        int r = arr.size() - 1;
-        while(l + 1 < arr.size() - 1 && arr[l] < arr[l + 1]){
-            l++;
+        int i = 0;
+        int j = arr.size() - 1;
+        while(i + 1 < arr.size() - 1 && arr[i] < arr[i + 1]){
+            i++;
         }
-        while(r - 1 > 0 && arr[r] < arr[r - 1]){
-            r--;
+        while(j - 1 > 0 && arr[j] < arr[j - 1]){
+            j--;
         }
-        return l == r;
+        return i == j;
     }
 };
