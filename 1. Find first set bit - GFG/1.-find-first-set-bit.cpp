7a60b8cf28406ddd7a348fdac6e5d8d7c650940b
Nawaz Warsi <nawaz.khan2525@gmail.com>
2022-03-30 19:23:26 +0530

    Added solution - LeetHub

--- 1. Find first set bit - GFG/1.-find-first-set-bit.cpp
+++ 1. Find first set bit - GFG/1.-find-first-set-bit.cpp
@@ -16,7 +16,8 @@ class Solution
     unsigned int getFirstSetBit(int n)
     {
         // Your code here
-         return log2(n & -n) + 1;
+         //return log2(n & -n) + 1;
+         return ffs(n);
     }
 };
 
