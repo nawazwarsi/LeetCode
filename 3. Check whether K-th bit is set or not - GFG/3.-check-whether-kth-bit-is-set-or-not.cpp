7c886b06fc5044f77b5316b312caa5ca80789a1a
Nawaz Warsi <nawaz.khan2525@gmail.com>
2022-03-30 18:48:05 +0530

    Added solution - LeetHub

--- 3. Check whether K-th bit is set or not - GFG/3.-check-whether-kth-bit-is-set-or-not.cpp
+++ 3. Check whether K-th bit is set or not - GFG/3.-check-whether-kth-bit-is-set-or-not.cpp
@@ -17,8 +17,7 @@ class Solution
     {
         // Your code here
         // It can be a one liner logic!! Think of it!!
-        if(n&(1<<k)) return true;
-        else return false;
+        return (n>>k)&1;
     }
 };
 
