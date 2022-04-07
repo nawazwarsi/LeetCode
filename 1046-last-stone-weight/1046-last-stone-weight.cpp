0cfdb2efde597da3977e967fce00be94bfcf4ae9
Nawaz Warsi <nawaz.khan2525@gmail.com>
2022-04-07 13:01:13 +0530

    Time: 4 ms (37.54%), Space: 7.6 MB (34.27%) - LeetHub

---
+++ 1046-last-stone-weight/1046-last-stone-weight.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int lastStoneWeight(vector<int>& stones) {
+        sort(stones.begin(), stones.end());
+        for(int i=stones.size()-1; i>0; i--){
+            stones[i-1]=stones[i]-stones[i-1];
+            sort(stones.begin(), stones.end());
+        }
+        return stones[0];
+    }
+};
