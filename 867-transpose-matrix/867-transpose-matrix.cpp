7bfc00cd35a779d488031621c778ea095a7660e9
Nawaz Warsi <nawaz.khan2525@gmail.com>
2022-06-03 00:14:53 +0530

    Time: 19 ms (34.70%), Space: 10.7 MB (43.03%) - LeetHub

---
+++ 867-transpose-matrix/867-transpose-matrix.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    vector<vector<int>> transpose(vector<vector<int>>& matrix) {
+         vector<vector<int>> new_matrix(matrix[0].size(), vector<int>(matrix.size(),0));
+        for(int i=0; i<matrix[0].size(); i++){
+            for(int j=0; j<matrix.size(); j++){
+                new_matrix[i][j]=matrix[j][i];
+            }
+        }
+        return new_matrix;
+    }
+};
