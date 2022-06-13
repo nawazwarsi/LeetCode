2d97f3ba398642740b5ad85a124e28c86962c27f
Nawaz Warsi <nawaz.khan2525@gmail.com>
2022-06-13 15:16:02 +0530

    Time: 4 ms (37.32%), Space: 6 MB (25.12%) - LeetHub

--- 231-power-of-two/231-power-of-two.cpp
+++ 231-power-of-two/231-power-of-two.cpp
@@ -1,16 +1,9 @@
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
-        if(n==1){
-            return true;
-        }
         if(n<=0){
             return false;
         }
-        bool flag=false;
-        if(n%2==0){
-            flag=true;
-        }
-        return flag&&isPowerOfTwo(n/2);
+        return ((n&(n-1))==0);
     }
 };
