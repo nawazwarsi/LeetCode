f85440c4ca0465114390279791c3727732848dca
Nawaz Warsi <nawaz.khan2525@gmail.com>
2023-01-24 10:16:59 +0530

    Time: 3 ms (40.71%), Space: 6 MB (22.93%) - LeetHub

---
+++ 2269-find-the-k-beauty-of-a-number/2269-find-the-k-beauty-of-a-number.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int divisorSubstrings(int num, int k) {
+        string s = to_string(num);
+        int count=0;
+        for(int i=0; i<=s.length()-k; i++){
+            int temp = stoi(s.substr(i,k));
+            if(temp && (num%temp==0)){
+                count++;
+            }
+        }
+        return count;
+    }
+};
+
+
