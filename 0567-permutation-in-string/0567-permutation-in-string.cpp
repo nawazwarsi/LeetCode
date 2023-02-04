b67da346765924f6eba6469b33f0b8c7251f49cb
Nawaz Warsi <nawaz.khan2525@gmail.com>
2023-02-04 09:16:55 +0530

    Time: 7 ms (77.33%), Space: 7.4 MB (32.35%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.cpp
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -1,26 +1,17 @@
 class Solution {
 public:
     bool checkInclusion(string s1, string s2) {
-        int sizeofs1=s1.size();
-        int sizeofs2=s2.size();
-        
-        if(sizeofs1>sizeofs2)  return false;
-        
-        vector<int>check1(26,0);   //Creating vector for store the  frequency of s1
-        vector<int>check2(26,0);     //Creating vector for store the  frequency of s2
-        
-        for(int i=0;i<sizeofs1;i++){
-            check1[s1[i] - 'a']++;   //Store the frequncy of s1
-            check2[s2[i] - 'a']++;  //Store the frequncy of s2
+        vector<int> a(128);
+        vector<int> b(128);
+        for(int i=0; i<s1.size(); i++){
+            a[s1[i]]++;
         }
-         if(check1==check2){   // If these vectors are equal then we need to return true
-            return true;
-        }
-        
-        for(int i=sizeofs1;i<sizeofs2;i++){
-            check2[s2[i-sizeofs1] - 'a']--;     //Remove the [i-sizeofs1]th element from check2
-            check2[s2[i] - 'a']++;                 //Adding the current element in check2
-            if(check1==check2) return true;   //If these vectors are equal then we need to return true
+        for(int i=0; i<s2.size(); i++){
+            b[s2[i]]++;
+            if(i>=s1.size()){
+                 b[s2[i-s1.size()]]--;
+            }
+            if(a == b) return true;
         }
         return false;
     }
