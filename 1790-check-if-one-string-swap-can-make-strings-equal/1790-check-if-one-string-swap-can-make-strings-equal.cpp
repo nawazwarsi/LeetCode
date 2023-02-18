33c4a6c2a31229c8be8acc486143068322296b38
Nawaz Warsi <nawaz.khan2525@gmail.com>
2023-02-18 15:48:30 +0530

    Time: 5 ms (14.80%), Space: 6.3 MB (23.20%) - LeetHub

---
+++ 1790-check-if-one-string-swap-can-make-strings-equal/1790-check-if-one-string-swap-can-make-strings-equal.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool areAlmostEqual(string s1, string s2) {
+        int count=0;
+        string t1 = s1;
+        sort(t1.begin(), t1.end());
+        string t2 = s2;
+        sort(t2.begin(), t2.end());
+        if(t1!=t2){
+            return false;
+        }
+        for(int i=0; i<s1.length(); i++){
+            if(s1[i]!=s2[i]){
+                count++;
+                if(count>2){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+};
