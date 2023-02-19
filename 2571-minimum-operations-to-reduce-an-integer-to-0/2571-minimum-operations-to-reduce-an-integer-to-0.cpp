0e9e5e35ac42bb63e82ab195d5490bb6188ccb34
Nawaz Warsi <nawaz.khan2525@gmail.com>
2023-02-19 13:34:06 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (14.29%) - LeetHub

--- 2571-minimum-operations-to-reduce-an-integer-to-0/2571-minimum-operations-to-reduce-an-integer-to-0.cpp
+++ 2571-minimum-operations-to-reduce-an-integer-to-0/2571-minimum-operations-to-reduce-an-integer-to-0.cpp
@@ -4,11 +4,15 @@ public:
         int nearPower = log2(n);
         int a = pow(2, nearPower);
         int b = pow(2, nearPower + 1);
-        if((n - a == 0) || (b - n == 0)){
+        
+        int low = n-a;
+        int high = b-n;
+        
+        if((low == 0) || (high == 0)){
             return 1;
         } 
-        int temp1 = minOperations(n - a) + 1;
-        int temp2 = minOperations(b - n) + 1;
+        int temp1 = minOperations(low) + 1;
+        int temp2 = minOperations(high) + 1;
         int ans = min(temp1, temp2);
         return ans;
     }
